d4f59e04804c537fb6f77a53eb0aa2c4f5b07ef4
Technical Krupesh <69968838+technicalkrupesh14@users.noreply.github.com>
2020-09-19 10:08:39 +0530

    Add files via upload

---
+++ HELLO_C.C
@@ -0,0 +1,11 @@
+//documentation section start
+#include<stdio.h>
+#include<conio.h>
+//documentation section over
+
+void main()
+{
+ clrscr();
+ printf("WELCOME TO C LANGUAGE WORLD"); //this is printing the text
+ getch();
+}
