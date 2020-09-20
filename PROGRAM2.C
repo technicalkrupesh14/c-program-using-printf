82b7b9835fba40871d34c9c77b5dee4f7c432cbf
Technical Krupesh <69968838+technicalkrupesh14@users.noreply.github.com>
2020-09-20 18:26:36 +0530

    Add files via upload

---
+++ PROGRAM2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+printf("\n * * * * * * * * * * * * * *");
+printf("\n *   KRUPESH VITHLANI      *");
+printf("\n *     17 year old         *");
+printf("\n *      JUNAHGADH          *");
+printf("\n *       362001            *");
+printf("\n * * * * * * * * * * * * * *");
+
+getch();
+}
