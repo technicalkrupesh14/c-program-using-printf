9fc4f6d197ead8ca86a6c6d8a68a2686c13b74f0
Technical Krupesh <69968838+technicalkrupesh14@users.noreply.github.com>
2020-09-24 15:20:50 +0530

    Add files via upload

---
+++ visiting_card.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// #include <conio.h>
+
+// void main()
+int main(){
+// clrscr();
+
+printf("\n * * * * * * * * * * * * * * * * * * * * * *");
+printf("\n * Krupesh - 67548 92356                   *");
+printf("\n *           57489 46380                   *");
+printf("\n *                                         *");
+printf("\n *          Krishna All In One Shop        *");
+printf("\n *-----------------------------------------*");
+printf("\n *       09, Raj Complex, Near SBI Bank    *");
+printf("\n *     MG Road, Kalwa Chowcj, Mumbai (MH)  *");
+printf("\n * * * * * * * * * * * * * * * * * * * * * *");
+    
+
+    return 0;
+    // getch();
+}
